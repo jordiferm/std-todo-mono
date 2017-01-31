cecb88d3729ffd71fe66641067f4cf1d0361bffb
Shadow <j@softtopia.com>
2017-02-01 00:35:52 +0100

    Added a service to access to items data and an item Entity class

---
+++ std-todo/domain/TodoItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace stdtodo
+{
+	public class TodoItem
+	{
+		String name; 
+		Boolean isComplete; 
+
+
+		public TodoItem ()
+		{
+			IsComplete = false; 
+		}
+
+		public TodoItem(String itemName) : this() {
+			Name = itemName;
+		}
+
+		public String Name {
+			get { return name; }
+			set { name = value;  }
+		}
+
+		public Boolean IsComplete {
+			get { return isComplete; }
+			set { isComplete = value; }
+		}
+
+	}
+}
+
---
+++ std-todo/service/TodoItemsService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stdtodo
+{
+	public class TodoItemsService
+	{
+		List<TodoItem> todoItemList = new List<TodoItem>(); 
+
+		public TodoItemsService ()
+		{
+			todoItemList.Add(new TodoItem("First TODO Item")); 
+			todoItemList.Add(new TodoItem("Second TODO Item")); 
+			todoItemList.Add(new TodoItem("Third TODO Item")); 
+			todoItemList.Add(new TodoItem("Forth TODO Item")); 
+			todoItemList.Add(new TodoItem("Fifth TODO Item")); 
+		}
+
+		public List<TodoItem>  findAllItems() {
+			//TODO: Get from RESTFUL; 
+			return todoItemList; 
+		}
+	}
+}
+
